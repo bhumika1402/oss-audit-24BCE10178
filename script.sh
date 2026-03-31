b4833f13d22b01138e8fd0201df1bbca5a49a679
bhumika1402 <bhumika.24bce10178@vitbhopal.ac.in>
2026-03-31 22:16:16 +0530

    System identity

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script 1: System Identity Report
+
+STUDENT_NAME="Bhumika Gupta"
+SOFTWARE_CHOICE="Python"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME="Not available in Git Bash"
+DISTRO="Git Bash Environment"
+
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2)
+
+echo "================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "================================"
+echo "Software : $SOFTWARE_CHOICE"
+echo "Kernel   : $KERNEL"
+echo "User     : $USER_NAME"
+echo "Uptime   : $UPTIME"
+echo "Date     : $DATE"
+echo "Distro   : $DISTRO"
+echo "License  : Linux is mostly GPL licensed"
